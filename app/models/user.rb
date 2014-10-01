2028aafe2b9392ad4a09ee17c1307508b934d92c
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2014-10-01 09:00:45 -0700

    fix typo so signing in multiple times via one twitter username works

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.id).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.nickname
